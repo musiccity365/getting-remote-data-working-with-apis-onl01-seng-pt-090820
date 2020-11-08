b569a7a47ce5deb6aaab6d427a06997dc8df8b5b
musiccity365 <m_sackstein@yahoo.com>
2020-11-08 18:53:20 -0500

    Done.

--- lib/nyc_api.rb
+++ lib/nyc_api.rb
@@ -2,24 +2,25 @@ require 'net/http'
  require 'open-uri'
  require 'json'
  
- class GetPrograms
+class GetPrograms
 
-  URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
+    URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
 
-  def get_programs
-    uri = URI.parse(URL)
-    response = Net::HTTP.get_response(uri)
-    response.body
-  end
-
-  def program_school
-    programs = JSON.parse(self.get_programs)
-    programs.collect do |program|
-      program["agency"]
+    def get_programs
+      uri = URI.parse(URL)
+      response = Net::HTTP.get_response(uri)
+      response.body
     end
-  end
 
+    def program_school
+        programs = JSON.parse(self.get_programs)
+        programs.collect do |program|
+          program["agency"]
+        end
+    end 
 end
 
- programs = GetPrograms.new.get_programs
- puts programs
+#  programs = GetPrograms.new.get_programs
+#  puts programs
+programs = GetPrograms.new
+puts programs.program_school.uniq
